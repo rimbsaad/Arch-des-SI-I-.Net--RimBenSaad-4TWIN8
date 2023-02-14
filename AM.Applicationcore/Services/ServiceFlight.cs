ebee355defd92edccefc985ab3e207085b5308d0
rimbsaad <rim.bensaad@esprit.tn>
2023-02-14 03:22:47 +0100

    update serviceFlight

--- AM.Applicationcore/Services/ServiceFlight.cs
+++ AM.Applicationcore/Services/ServiceFlight.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace AM.ApplicationCore.Services
 {
@@ -36,7 +37,7 @@ namespace AM.ApplicationCore.Services
                     yield return flight.FlightDate;
                 }
             }
-        }*/
+        }
 
         IEnumerable<DateTime> GetFlightDates(string destination)
         {
@@ -48,66 +49,140 @@ namespace AM.ApplicationCore.Services
                 }
             }
         }
-
-        public void GetFlights(string filterType, string filterValue)
+        IEnumerable<DateTime> GetFlightDates1(string destination) { 
+        IEnumerable<DateTime> query =
+           from f in Flights
+           where f.Destination == destination
+           select f.FlightDate;
+            return query;
+       }
+        IEnumerable<DateTime> GetFlightDates2(string destination)
         {
-            switch (filterType)
+            return Flights.Where(f=> f.Destination == destination).Select(f => f.FlightDate);
+        }
+        void ShowFlightDetails(Plane plane)
+        {
+            var query = from p in Flights
+                        where p.plane.PlainId== plane.PlainId   
+                        select p;
+
+            foreach (var item in query)
             {
-                case "Destination":
-                    {
-                        var result = Flights.Where(f => f.Destination == filterValue).ToList();
-                        foreach (var f in result)
-                        {
-                            Console.WriteLine(f);
-                        }
-                    }
-                    break;
-                case "Departure":
-                    {
-                        var result = Flights.Where(f => f.Departure == filterValue).ToList();
-                        foreach (var f in result)
-                        {
-                            Console.WriteLine(f);
-                        }
-                    }
-                    break;
-                case "FlightDate":
-                    {
-                        var result = Flights.Where(f => f.FlightDate == DateTime.Parse(filterValue)).ToList();
-                        foreach (var f in result)
-                        {
-                            Console.WriteLine(f);
-                        }
-                    }
-                    break;
-                case "FlightId":
-                    {
-                        var result = Flights.Where(f => f.FlightId == int.Parse(filterValue)).ToList();
-                        foreach (var f in result)
-                        {
-                            Console.WriteLine(f);
-                        }
-                    }
-                    break;
-                case "EffectiveArrival":
-                    {
-                        var result = Flights.Where(f => f.EffectiveArrival == DateTime.Parse(filterValue)).ToList();
-                        foreach (var f in result)
-                        {
-                            Console.WriteLine(f);
-                        }
-                    }
-                    break;
-                case "EstimatedDuration":
-                    {
-                        var result = Flights.Where(f => f.EstimatedDuration == int.Parse(filterValue)).ToList();
-                        foreach (var f in result)
-                        {
-                            Console.WriteLine(f);
-                        }
-                    }
-                    break;
+                Console.WriteLine(item.FlightDate+item.Destination);
             }
         }
+        void ShowFlightDetails1(Plane plane)
+        {
+
+            return Flights.Where(f => f.plane.PlainId == plane.PlainId).Select(f => f.FlightDate + f.Destination).ToString();
+        }
+        public int   ProgrammedFlightNumber(DateTime startDate)
+        {
+            var query = (from f in Flights
+                        where (startDate - f.FlightDate).TotalDays < 7 && startDate <= f.FlightDate
+                        select f).Count();
+            return query;
+
+            return Flights.Count(f => (startDate - f.FlightDate).TotalDays < 7 && startDate <= f.FlightDate);
+
+          }
+        //     return Flights.Count(f => f.flightDate<=startDate.adddays(7) && startDate <= f.FlightDate);
+
+        public double DurationAverage(string destination)
+        {
+            return Flights.Where(f => f.Destination == destination).Average(f=>f.EstimatedDuration);
+        }
+        IEnumerable<Flight> OrderedDurationFlights()
+        {
+            return Flights.OrderByDescending(f=>f.EstimatedDuration);
+        }*/
+
+
+        public static IEnumerable<Traveller> SeniorTravellers(Flight flight)
+        {
+            List<Traveller> Travellers = flight.Passengers.OfType<Traveller>().ToList();
+            return Travellers.OrderBy(t => t.BirthDate).Take(3);
+
+            //return Flights.Select(f => f.Passengers.OfType<Traveller>()).OrderBy((f, keySelector ) => Func(f, keySelector);
+
+        }
+
+
+
+
+
+        //public void GetFlights(string filterType, string filterValue)
+        //{
+        //    switch (filterType)
+        //    {
+        //        case "Destination":
+        //            {
+        //                var result = Flights.Where(f => f.Destination == filterValue).ToList();
+        //                foreach (var f in result)
+        //                {
+        //                    Console.WriteLine(f);
+        //                }
+        //            }
+        //            break;
+        //        case "Departure":
+        //            {
+        //                var result = Flights.Where(f => f.Departure == filterValue).ToList();
+        //                foreach (var f in result)
+        //                {
+        //                    Console.WriteLine(f);
+        //                }
+        //            }
+        //            break;
+        //        case "FlightDate":
+        //            {
+        //                var result = Flights.Where(f => f.FlightDate == DateTime.Parse(filterValue)).ToList();
+        //                foreach (var f in result)
+        //                {
+        //                    Console.WriteLine(f);
+        //                }
+        //            }
+        //            break;
+        //        case "FlightId":
+        //            {
+        //                var result = Flights.Where(f => f.FlightId == int.Parse(filterValue)).ToList();
+        //                foreach (var f in result)
+        //                {
+        //                    Console.WriteLine(f);
+        //                }
+        //            }
+        //            break;
+        //        case "EffectiveArrival":
+        //            {
+        //                var result = Flights.Where(f => f.EffectiveArrival == DateTime.Parse(filterValue)).ToList();
+        //                foreach (var f in result)
+        //                {
+        //                    Console.WriteLine(f);
+        //                }
+        //            }
+        //            break;
+        //        case "EstimatedDuration":
+        //            {
+        //                var result = Flights.Where(f => f.EstimatedDuration == int.Parse(filterValue)).ToList();
+        //                foreach (var f in result)
+        //                {
+        //                    Console.WriteLine(f);
+        //                }
+        //            }
+        //            break;
+        //    }
+        //}
+
+
+        //public void GetFlights1(string filterValue, Func<string, Flight, bool> condition)
+        //{
+
+        //    foreach (var f in Flights)
+        //    {
+        //        if (condition(filterValue, f))
+        //            Console.WriteLine(f);
+        //    }
+
+        //}
+
     }
 }
